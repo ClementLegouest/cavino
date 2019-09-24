ac0a1bda0f278c9b7f7be4a88c5550b59319a838
ClementLegouest <clement.legouest@gmail.com>
2019-09-24 23:06:52 +0200

    [evo] domain service

---
+++ src/app/services/domain.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {EnvService} from './env.service';
+import {AuthService} from './auth.service';
+import {Observable} from 'rxjs';
+import {Region} from '../models/region';
+import {Domain} from '../models/domain';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DomainService {
+
+  constructor(
+      private http: HttpClient,
+      private env: EnvService,
+      private auth: AuthService,
+  ) { }
+
+  getAllDomains(): Observable<Array<Domain>> {
+    const bearer = 'Bearer ' + this.auth.token.token;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        accept: 'application/json',
+        Authorization: bearer,
+      })
+    };
+    return this.http.get<Array<Domain>>(this.env.GETALLREGIONSURL, httpOptions);
+  }
+}
